08901a2b4a4a26247ac35b0eac6a229ab5fbdf02
EwanMcHenry <ewan.mchenry@outlook.com>
2024-05-28 19:33:03 +0100

    putting constants in a list

--- subparts of calculation/sub01.2- setting model constants 01.R
+++ subparts of calculation/sub01.2- setting model constants 01.R
@@ -1,40 +1,40 @@
 ##------ Wed Aug 31 14:12:23 2022 ------##
 # functional connectivity metric dev
 # sub 01.2 - setting constants
-
-
+read.csv(paste0(func.conect.path, "\\delphi point estimate summary.csv"))
 
 
 # SET MODEL CONSTANTS ----
-# patch identification
-# often single real life "patch" might be split by 1-2 cells in teh data, this is the distance to join such split-patches into one
-buffer.for.patchid = 25 # distance to buffer around LCM patchest to define patch ID, this is 1/2 the max distance separating clumps within the same patch
+constants <- list(
+  ## patch identification ----
+  # often single real life "patch" might be split by 1-2 cells in teh data, this is the distance to join such split-patches into one
+  buffer.for.patchid = 25, # distance to buffer around LCM patchest to define patch ID, this is 1/2 the max distance separating clumps within the same patch
 
-# patch quality modifiers  - taken from delphi expert opinion
-read.csv(paste0(func.conect.path, "\\delphi point estimate summary.csv"))
-relative.edge.quality = 0.6 # relative value of edge habitat compared to core(1)
-non.awi.qual.eff = 1 # relative value of non awi
-awi.qual.eff =  1/0.45 # relative value of awi within patches
-
-# dispersal parameters - just change the dispersal.dist.95
-dispersal.dist.95  = 5000 # "max" dispersal distance (95th percentile)
-alpha  = -log(0.05)/dispersal.dist.95 # dispersal kernal scaleing parameter
-max.dispersal.considered  = dispersal.dist.95  * 2 # to limit max dispersal distance between patches for computationall efficiency
-max.p.considered = exp(-alpha * max.dispersal.considered)
-mean.effective.dispersal = -log(0.5)/alpha
-
-#another set of dispersal distances 
-dispersal.dist.95.sensitive = 1000
-alpha.sensitive =  -log(0.05)/dispersal.dist.95.sensitive
-max.dispersal.considered.sensitive  = dispersal.dist.95.sensitive  * 2 # to limit max dispersal distance between patches for computationall efficiency
-max.p.considered.sensitive = exp(-alpha * max.dispersal.considered.sensitive)
-mean.effective.dispersal.sensitive = -log(0.5)/alpha.sensitive
-
-buffer.roundLandscape = max.dispersal.considered # buffer around landscape, to account for contribution of neighboring habitat
-
-# dispersal cost
-focalhab.cost = 0.05 # cost of moving through focal (bl woodland) habitat - given nominal amount to protect against imapct of super thin corridors
-cost.res = 4 # n cells to be mean-aggregated (vert and horiz) for dispersal cost. higher to reduce computing time
+  ## patch quality modifiers  - taken from delphi expert opinion
+  relative.edge.quality = 0.6, # relative value of edge habitat compared to core(1)
+  non.awi.qual.eff = 1, # relative value of non awi
+  awi.qual.eff =  1/0.45, # relative value of awi within patches
+  
+  # dispersal parameters - just change the dispersal.dist.95
+  dispersal.dist.95  = 5000, # "max" dispersal distance (95th percentile)
+  multiple.max.considered = 2, # factor to limit patch searching to an absolute max dispersal distance for computationall efficiency
+  alpha  = -log(0.05)/dispersal.dist.95 # dispersal kernal scaleing parameter
+  max.dispersal.considered  = dispersal.dist.95  * multiple.max.considered # to limit max dispersal distance between patches for computationall efficiency
+  max.p.considered = exp(-alpha * max.dispersal.considered)
+  mean.effective.dispersal = -log(0.5)/alpha
+  
+  #another set of dispersal distances 
+  dispersal.dist.95.sensitive = 1000
+  alpha.sensitive =  -log(0.05)/dispersal.dist.95.sensitive
+  max.dispersal.considered.sensitive  = dispersal.dist.95.sensitive  * multiple.max.considered # to limit max dispersal distance between patches for computationall efficiency
+  max.p.considered.sensitive = exp(-alpha * max.dispersal.considered.sensitive)
+  mean.effective.dispersal.sensitive = -log(0.5)/alpha.sensitive
+  
+  buffer.roundLandscape = max.dispersal.considered # buffer around landscape, to account for contribution of neighboring habitat
+  
+  # dispersal cost
+  focalhab.cost = 0.05 # cost of moving through focal (bl woodland) habitat - given nominal amount to protect against imapct of super thin corridors
+  cost.res = 4 # n cells to be mean-aggregated (vert and horiz) for dispersal cost. higher to reduce computing time
 
 # define which cost set is being used here
 eycott = read.csv("hab costs and edge effects Eycott 2011.csv")
