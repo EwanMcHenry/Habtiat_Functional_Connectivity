577b2effc6c1513388cc84a77d08ac0a05e619e5
EwanMcHenry <ewan.mchenry@outlook.com>
2026-05-26 11:34:06 +0100

    saved the hexgrid for loading

--- subparts of calculation/sub02.1 - curate patch_agnostic.R
+++ subparts of calculation/sub02.1 - curate patch_agnostic.R
@@ -3,4 +3,13 @@ hex.grid0 = st_make_grid(countries, c(constants$hexdist.h, constants$hexdist.v),
 hex.grid = st_sf(hex.grid0) %>%
   # add grid ID
   mutate(grid_id = 1:length(lengths(hex.grid0)))
+rm(hex.grid0)
+gc()
 
+
+save(hex.grid, 
+     file = paste0(func.conect.path, 
+                   "\\analysis outputs\\r_curated_global_data_.RData")
+)
+
+print("script02.01 done")
