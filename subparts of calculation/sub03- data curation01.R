3f2104f3cb508cf7cf789050372205fc62491fbb
EwanMcHenry <ewan.mchenry@outlook.com>
2026-05-26 11:35:53 +0100

    adding lcm loading and masking in here

--- subparts of calculation/sub03- data curation01.R
+++ subparts of calculation/sub03- data curation01.R
@@ -11,10 +11,10 @@
 this.ts = Focal_landscape[this.ts.num,]
 ts.buff = this.ts %>% 
   st_simplify( preserveTopology = T, dTolerance = constants$landscape.buffer.simplification.tolerance[1]) %>% # first simplify hack to reduce run time. this is a rough buffer to negate edge effects, so can be v rough
-  st_buffer( dist = constants$buffer.roundLandscape) %>% # buffer
+  st_buffer( dist = constants$max.dispersal.considered) %>% # buffer
   st_simplify( preserveTopology = FALSE, dTolerance = constants$landscape.buffer.simplification.tolerance[2]) # simplify again
 ### name of country ----
-this.country = st_intersection( st_point_on_surface(this.ts) , countries)$name.1 
+this.country = st_intersection( st_point_on_surface(this.ts) , countries)$nation 
 
 ### select hex grid ----
 
@@ -30,105 +30,94 @@ ts.hexgrid$hex.ha = st_area(ts.hexgrid) %>%
   set_units(value = "ha") %>%
   as.numeric()
 
-### select lcm and transform crs of other data if needed----
-# different lcm for NI/GB
-if(this.country == "N.Ireland"){
-  
-  lcm_tplus1.rast25 = lcm_tplus1.rast25.ni #%>% projectRaster(., crs = crs(lcm_tplus1.rast25.gb), method = "ngb")
-  lcm_t.rast25 = lcm_t.rast25.ni #%>% projectRaster(., crs = crs(lcm_t.rast25.gb), method = "ngb")
 
-  ## cut LCM to landscape
-  tscrop.lcm_tplus1.rast25 <- crop(lcm_tplus1.rast25, extent(ts.buff %>% st_transform(29903) ))
-  tsbuff.lcm_tplus1.rast25 <- fast_mask(tscrop.lcm_tplus1.rast25, ts.buff %>% st_transform(29903))
-  
-  tscrop.lcm_t.rast25 <- crop(lcm_t.rast25, extent(ts.buff %>% st_transform(29903)))
-  tsbuff.lcm_t.rast25 <- fast_mask(tscrop.lcm_t.rast25, ts.buff %>% st_transform(29903))
-  
-  } else{
-  lcm_tplus1.rast25 = lcm_tplus1.rast25.gb
-  lcm_t.rast25 = lcm_t.rast25.gb
+# transform landscape and data to crs ----
 
-  ## cut LCM to landscape
-  tscrop.lcm_tplus1.rast25 <- crop(lcm_tplus1.rast25, extent(ts.buff))
-  tsbuff.lcm_tplus1.rast25 <- fast_mask(tscrop.lcm_tplus1.rast25, ts.buff)
-  
-  tscrop.lcm_t.rast25 <- crop(lcm_t.rast25, extent(ts.buff))
-  tsbuff.lcm_t.rast25 <- fast_mask(tscrop.lcm_t.rast25, ts.buff)
-  
+crs_use <- if (this.country == "Northern Ireland") {
+  29903
+} else {
+  27700
 }
 
-# Create a named list of rasters, keyed by year
+awi <- st_transform(awi, crs_use)
+nwss <- st_transform(nwss, crs_use)
 
-raster.list <- setNames(
-  list(
-    tsbuff.lcm_t.rast25,
-    tsbuff.lcm_tplus1.rast25
-  ),
-  sort(years.considered)
+this.ts <- st_transform(this.ts, crs_use)
+ts.buff <- st_transform(ts.buff, crs_use)
+tsbuff.hexgrid <- st_transform(tsbuff.hexgrid, crs_use)
+ts.hexgrid <- st_transform(ts.hexgrid, crs_use)
+
+
+### select and mask lcm ----
+## select years and corresponding index ----
+lcm <- load_lcm(
+  lcm.directs = lcm.directs,
+  years = years.considered,
+  resolution = "25"
 )
 
-
-# Create lcm.landscape by explicitly matching years
-lcm.landscape <- lapply(years.considered, function(yr) {
-  list(year = yr, raster = raster.list[[as.character(yr)]])
-})
-
-# tscrop.lcm_tplus1.rast25.unpro <- crop(lcm_tplus1.rast25.unpro, extent(ts.buff))
-# tsbuff.lcm_tplus1.rast25.unpro <- fast_mask(tscrop.lcm_tplus1.rast25.unpro, ts.buff)
-# 
-# tscrop.lcm_t.rast25.unpro <- crop(lcm_t.rast25.unpro, extent(ts.buff))
-# tsbuff.lcm_t.rast25.unpro <- fast_mask(tscrop.lcm_t.rast25.unpro, ts.buff)
-
+lcm.landscape <- mask_lcm_landscape(
+  lcm = lcm,
+  landscape = ts.buff,
+  country = this.country
+)
 
 ### select awi ----
-tsbuff.awi <- st_intersection(awi, ts.buff) %>% 
+awi.landscape <- st_intersection(awi, ts.buff) %>% 
   st_make_valid() %>%  st_cast("MULTIPOLYGON") %>% 
   st_cast("POLYGON")
 
 ### select nwss with more than 50% native canopy and rasterise with ts ----
 
 if(this.country == "Scotland"){
-  tsbuff.nwss <- st_intersection(nwss[nwss$NATIVE_PCT>=50,] %>% st_cast("POLYGON"), ts.buff) %>% 
+  nwss.landscape <- st_intersection(nwss[nwss$NATIVE_PCT>=50,] %>% st_cast("POLYGON"), ts.buff) %>% 
     st_make_valid() %>%  st_cast("MULTIPOLYGON") %>% 
     st_cast("POLYGON")
-} else{ tsbuff.nwss = NA}
+} else{ nwss.landscape = NA}
 
-### rasterise layers with same config as lcm
-tsbuff.rast = fasterize(ts.buff, tsbuff.lcm_tplus1.rast25)
-tsbuff.awi.raster = fasterize(tsbuff.awi %>% st_as_sf(), tsbuff.lcm_tplus1.rast25)
-if(this.country == "Scotland"){
-  tsbuff.nwss.raster = fasterize(tsbuff.nwss %>% st_as_sf(), tsbuff.lcm_tplus1.rast25) %>% 
-  buffr (., distance = 25, units = "geographic", target_value = 1)
-} else{ tsbuff.nwss.raster = NA}
-
-awi.landscape = tsbuff.awi#eg.awi
-nwss.landscape = tsbuff.nwss.raster
-
+# ### rasterise layers with same config as lcm
+# tsbuff.rast = fasterize(ts.buff, tsbuff.lcm_tplus1.rast25)
+# tsbuff.awi.raster = fasterize(tsbuff.awi %>% st_as_sf(), tsbuff.lcm_tplus1.rast25)
+# if(this.country == "Scotland"){
+#   tsbuff.nwss.raster = fasterize(tsbuff.nwss %>% st_as_sf(), tsbuff.lcm_tplus1.rast25) %>% 
+#   buffr (., distance = 25, units = "geographic", target_value = 1)
+# } else{ tsbuff.nwss.raster = NA}
+# 
+# awi.landscape = tsbuff.awi#eg.awi
+# nwss.landscape = tsbuff.nwss.raster
+# 
 
 
 ### save ----
 save(awi.landscape, 
      nwss.landscape,
+     
+     tsbuff.hexgrid,
+     ts.hexgrid,
+     this.ts,
+     
+     this.country,
+     
+     lcm.landscape,
+     
+     
+     # tsbuff.awi, 
+     
   #tsbuff.lcm_tplus1.rast25,
      #tsbuff.lcm_t.rast25,
      # tsbuff.lcm_tplus1.rast25.unpro, 
      # tsbuff.lcm_t.rast25.unpro, 
-     tsbuff.awi, 
      #tsbuff.nwss,
-     tsbuff.nwss.raster,
+     # tsbuff.nwss.raster,
      # tsbuff.rast,
     # tsbuff.awi.raster,
-     tsbuff.hexgrid,
-     ts.hexgrid,
-     this.ts,
-     this.country,
-    lcm.landscape,
      file = paste0(func.conect.path, 
                    "\\analysis outputs\\", this.tss[this.ts.num], "\\r_curated data_.RData")
     )
 
 # save some space in RAM by removing some objects ----
-rm(awi, lcm_tplus1.rast25.ni,hex.grid0, hex.grid, nwss, 
+rm(countries,
+   awi, lcm_tplus1.rast25.ni,hex.grid0, hex.grid, nwss, 
    tsbuff.nwss, 
    tsbuff.nwss.raster,
    tsbuff.awi.raster, lcm_tplus1.rast25, lcm_t.rast25, tsbuff.rast, 
@@ -136,7 +125,17 @@ rm(awi, lcm_tplus1.rast25.ni,hex.grid0, hex.grid, nwss,
    tscrop.lcm_tplus1.rast25, tscrop.lcm_t.rast25, 
    lcm_tplus1.rast25.gb,
    lcm_t.rast25.ni,
-   lcm_t.rast25.gb#, 
+   lcm_t.rast25.gb,awi.landscape, 
+     nwss.landscape,
+     
+     tsbuff.hexgrid,
+     ts.hexgrid,
+     this.ts,
+     
+     this.country,
+     
+     lcm.landscape
+     #, 
    #tsbuff.lcm_tplus1.rast25.unpro, tsbuff.lcm_t.rast25.unpro
    )
 gc()
