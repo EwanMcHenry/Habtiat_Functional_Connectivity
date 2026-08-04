d0fada2d637ae61780c4cf91d18a90a6648e3f10
EwanMcHenry <ewan.mchenry@outlook.com>
2026-08-04 16:05:35 +0100

    Initial after speedup
    direcotry fixing
    
    contributations - fix to maths

--- subparts of calculation/sub07- total across landscapes - efective patch area and eca calculation 01.R
+++ subparts of calculation/sub07- total across landscapes - efective patch area and eca calculation 01.R
@@ -11,23 +11,54 @@
 # load data needed for landscapes in loop
 individ.ts.outputs = list (NA)
   for(y in seq_along(years.considered)){
+    this.year = years.considered[y]
     load(paste0(func.conect.path, 
-                "\\analysis outputs\\", this.tss[i], "\\", years.considered[y], "\\r_funcconnect_EffectiveAreas_ECAobs_.RData"))
+                "\\analysis outputs\\", this.tss[this.ts.num], "\\", this.year, "\\lcmres", constants$lcm.res, 
+                "\\06_r_funcconnect_EffectiveAreas_ECAobs_.RData"))
     # give an identifier for the landscape stuff came from
-    ts.hexgrid$ts.name = this.tss[i]
-    bl.patch.hexid.centroids$ts.name = this.tss[i]
+    ts.hexgrid$ts.name = this.tss
+    source_patch_centroid_info$ts.name = this.tss
     # hexgrid with stuff 
-    individ.ts.outputs[[(i-1)*length(years.considered)+y]] = list(name = this.tss[i], 
-                                                            year = years.considered[y],
-                                                            ts.hexgrid = ts.hexgrid, 
-                                                            bl.patch.hexid.centroids = bl.patch.hexid.centroids)
+    individ.ts.outputs[[this.tss]][[this.year]] = list(name = this.tss, 
+                                                       year = this.year,
+                                                       alpha = constants$alpha,
+                                                       costs = dispers.costs,
+                                                       cost.scale.factor = constants$cost.scale.factor,
+                                                       lcm_res = constants$lcm.res,
+                                                       hex_grid = ts.hexgrid,
+                                                       source_patch_centroid_info = source_patch_centroid_info,
+                                                       landscape_stats = c(
+                                                         name = this.tss,
+                                                         year = this.year,
+                                                         lcd.ECA = landscape_stats[[this.ts.num]][[this.year]]$lcd.ECA,
+                                                         euclid.ECA = landscape_stats[[this.ts.num]][[this.year]]$euclid.ECA,
+                                                         
+
+                                                         n.patch = landscape_stats[[this.ts.num]][[this.year]]$n.clumps,
+                                                         quality_weighted_habitat_ha_sum = landscape_stats[[this.ts.num]][[this.year]]$tot.patch.area.effective.ha,
+                                                         habitat_ha_sum = landscape_stats[[this.ts.num]][[this.year]]$tot.patch.ha,
+                                                         aw_habitat_ha_sum = landscape_stats[[this.ts.num]][[this.year]]$tot.aw.patch.ha,
+                                                         edge_habitat_ha_sum = landscape_stats[[this.ts.num]][[this.year]]$tot.edge.patch.ha,
+                                                         awi_edge_ha_sum = landscape_stats[[this.ts.num]][[this.year]]$tot.awi_edge.ha,
+                                                         
+                                                         med.patch.ha = landscape_stats[[this.ts.num]][[this.year]]$med.patch.ha,
+                                                         mean.patch.ha = landscape_stats[[this.ts.num]][[this.year]]$mean.patch.ha,
+                                                         largest_patch_ha = landscape_stats[[this.ts.num]][[this.year]]$largest_patch_ha,
+                                                         largest_patch_quality_weighted_ha =landscape_stats[[this.ts.num]][[this.year]]$largest_patch_quality_weighted_ha,
+                                                         
+                                                         
+                                                         ecolog.cost.not.sea_mean = landscape_stats[[this.ts.num]][[this.year]]$landscape.ecolog.cost.not.sea_mean,
+                                                         ecolog.cost.not.sea_median = landscape_stats[[this.ts.num]][[this.year]]$landscape.ecolog.cost.not.sea_median,
+                                                         ecolog.cost.not.sea_p70 = landscape_stats[[this.ts.num]][[this.year]]$landscape.ecolog.cost.not.sea_p70
+                                                         )
+                                                       )
   }
 
 # SET UP FOR LOOP FOR YEARS
 for (this.year in years.considered) {
   # pull together patch info from  all landscapes from that year
-  bl.patch.hexid.centroids = do.call(rbind,
-                                     map(individ.ts.outputs, "bl.patch.hexid.centroids")[map(individ.ts.outputs, "year") %>% unlist() == this.year])
+  source_patch_centroid_info = do.call(rbind,
+                                     map(individ.ts.outputs, "source_patch_centroid_info")[map(individ.ts.outputs, "year") %>% unlist() == this.year])
   ts.hexgrid = do.call(rbind,
                        map(individ.ts.outputs, "ts.hexgrid")[map(individ.ts.outputs, "year") %>% unlist() == this.year])
   # combine hexgrid info from same hexgrid from different treescapes ----
@@ -62,61 +93,61 @@ for (this.year in years.considered) {
   
   landscape.metrics = data.frame(name = ts.andAll.lcm.names[length(ts.andAll.lcm.names)],
                                  year = this.year,
-                                 leastcost.ECA =         sqrt(sum(bl.patch.hexid.centroids$leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T])),
-                                 # scaled.leastcost.ECA =  sqrt(sum(bl.patch.hexid.centroids$scaled.leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T])),
-                                 euclid.ECA = sqrt(sum(bl.patch.hexid.centroids$euclid.contrib.to[bl.patch.hexid.centroids$in.landscape == T])),
-                                 n.clumps = length(unique(bl.patch.hexid.centroids$clumps[bl.patch.hexid.centroids$in.landscape == T])),
-                                 tot.patch.ha = sum(bl.patch.hexid.centroids$subpatch.hex.ha [bl.patch.hexid.centroids$in.landscape == T]),
-                                 tot.aw.patch.ha = sum(bl.patch.hexid.centroids$awi.subpatch.hex.ha [bl.patch.hexid.centroids$in.landscape == T]),
-                                 tot.edge.patch.ha = sum(bl.patch.hexid.centroids$edge.subpatch.hex.ha [bl.patch.hexid.centroids$in.landscape == T]),
-                                 tot.awedge.patch.ha = sum(bl.patch.hexid.centroids$edge.awi.subpatch.hex.ha [bl.patch.hexid.centroids$in.landscape == T]),
-                                 med.patch.ha = median(bl.patch.hexid.centroids$clump.ha[!duplicated(bl.patch.hexid.centroids$clumps)])
+                                 leastcost.ECA =         sqrt(sum(source_patch_centroid_info$leastcost.contrib.to[source_patch_centroid_info$in.landscape == T])),
+                                 # scaled.leastcost.ECA =  sqrt(sum(source_patch_centroid_info$scaled.leastcost.contrib.to[source_patch_centroid_info$in.landscape == T])),
+                                 euclid.ECA = sqrt(sum(source_patch_centroid_info$euclid.contrib.to[source_patch_centroid_info$in.landscape == T])),
+                                 n.clumps = length(unique(source_patch_centroid_info$clumps[source_patch_centroid_info$in.landscape == T])),
+                                 tot.patch.ha = sum(source_patch_centroid_info$subpatch.hex.ha [source_patch_centroid_info$in.landscape == T]),
+                                 tot.aw.patch.ha = sum(source_patch_centroid_info$awi.subpatch.hex.ha [source_patch_centroid_info$in.landscape == T]),
+                                 tot.edge.patch.ha = sum(source_patch_centroid_info$edge.subpatch.hex.ha [source_patch_centroid_info$in.landscape == T]),
+                                 tot.awedge.patch.ha = sum(source_patch_centroid_info$edge.awi.subpatch.hex.ha [source_patch_centroid_info$in.landscape == T]),
+                                 med.patch.ha = median(source_patch_centroid_info$clump.ha[!duplicated(source_patch_centroid_info$clumps)])
   )
   landscape.metrics$mean.patch.ha = landscape.metrics$tot.patch.ha/ landscape.metrics$n.clumps
   
-  # sensitive.landscape.leastcost.ECA = sqrt(sum(bl.patch.hexid.centroids$sensitive.leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T]))
-  # sensitive.landscape.scaled.leastcost.ECA = sqrt(sum(bl.patch.hexid.centroids$sensitive.scaled.leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T]))
-  # sensitive.landscape.euclid.ECA = sqrt(sum(bl.patch.hexid.centroids$sensitive.euclid.contrib.to[bl.patch.hexid.centroids$in.landscape == T]))
+  # sensitive.landscape.leastcost.ECA = sqrt(sum(source_patch_centroid_info$sensitive.leastcost.contrib.to[source_patch_centroid_info$in.landscape == T]))
+  # sensitive.landscape.scaled.leastcost.ECA = sqrt(sum(source_patch_centroid_info$sensitive.scaled.leastcost.contrib.to[source_patch_centroid_info$in.landscape == T]))
+  # sensitive.landscape.euclid.ECA = sqrt(sum(source_patch_centroid_info$sensitive.euclid.contrib.to[source_patch_centroid_info$in.landscape == T]))
   
   # # ECA per grid cell ----
   # # sqrt of sum of all summed contributions of patches in the defined landscape by hex
-  # hex.sum.leastcost.contrib.to =  aggregate(bl.patch.hexid.centroids$leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T], # contrib to each patch
-  #                                           by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]),  # by hex
+  # hex.sum.leastcost.contrib.to =  aggregate(source_patch_centroid_info$leastcost.contrib.to[source_patch_centroid_info$in.landscape == T], # contrib to each patch
+  #                                           by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]),  # by hex
   #                                           function(x){sqrt(sum(x))}) %>% rename(grid_id = Group.1, hex.eca = x)
-  # hex.sum.scaled.leastcost.contrib.to =  aggregate(bl.patch.hexid.centroids$scaled.leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
-  #                                                  by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){ # over each grid
+  # hex.sum.scaled.leastcost.contrib.to =  aggregate(source_patch_centroid_info$scaled.leastcost.contrib.to[source_patch_centroid_info$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
+  #                                                  by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){ # over each grid
   #                                                    sqrt(sum(x))}) %>% rename(grid_id = Group.1, hex.eca = x)
-  # hex.sum.euclid.contrib.to =  aggregate(bl.patch.hexid.centroids$euclid.contrib.to[bl.patch.hexid.centroids$in.landscape == T], 
-  #                                        by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){
+  # hex.sum.euclid.contrib.to =  aggregate(source_patch_centroid_info$euclid.contrib.to[source_patch_centroid_info$in.landscape == T], 
+  #                                        by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){
   #                                          sqrt(sum(x))}) %>% rename(grid_id = Group.1, hex.eca = x)
-  # hex.n.clumps =  aggregate(bl.patch.hexid.centroids$clumps[bl.patch.hexid.centroids$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
-  #                           by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){ # over each grid
+  # hex.n.clumps =  aggregate(source_patch_centroid_info$clumps[source_patch_centroid_info$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
+  #                           by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){ # over each grid
   #                             length(unique(x))}) %>% rename(grid_id = Group.1, n.clumps = x)
-  # hex.tot.patch.ha =  aggregate(bl.patch.hexid.centroids$subpatch.hex.ha[bl.patch.hexid.centroids$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
-  #                               by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){ # over each grid
+  # hex.tot.patch.ha =  aggregate(source_patch_centroid_info$subpatch.hex.ha[source_patch_centroid_info$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
+  #                               by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){ # over each grid
   #                                 sum(x)}) %>% rename(grid_id = Group.1, tot.patch.ha = x)
-  # hex.tot.aw.patch.ha =  aggregate(bl.patch.hexid.centroids$awi.subpatch.hex.ha[bl.patch.hexid.centroids$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
-  #                                  by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){ # over each grid
+  # hex.tot.aw.patch.ha =  aggregate(source_patch_centroid_info$awi.subpatch.hex.ha[source_patch_centroid_info$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
+  #                                  by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){ # over each grid
   #                                    sum(x)}) %>% rename(grid_id = Group.1, tot.aw.patch.ha = x)
-  # hex.tot.edge.patch.ha =  aggregate(bl.patch.hexid.centroids$edge.subpatch.hex.ha[bl.patch.hexid.centroids$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
-  #                                    by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){ # over each grid
+  # hex.tot.edge.patch.ha =  aggregate(source_patch_centroid_info$edge.subpatch.hex.ha[source_patch_centroid_info$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
+  #                                    by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){ # over each grid
   #                                      sum(x)}) %>% rename(grid_id = Group.1, tot.edge.patch.ha = x)
-  # hex.tot.awedge.patch.ha =  aggregate(bl.patch.hexid.centroids$edge.awi.subpatch.hex.ha[bl.patch.hexid.centroids$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
-  #                                      by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){ # over each grid
+  # hex.tot.awedge.patch.ha =  aggregate(source_patch_centroid_info$edge.awi.subpatch.hex.ha[source_patch_centroid_info$in.landscape == T],  # scaled (so mean landscaep cost ==1) contribution to patch
+  #                                      by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){ # over each grid
   #                                        sum(x)}) %>% rename(grid_id = Group.1, tot.awedge.patch.ha = x)
   # 
   # 
   # 
-  # # sensitive.hex.sum.leastcost.contrib.to =  aggregate(bl.patch.hexid.centroids$sensitive.leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T], 
-  # #                                           by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){
+  # # sensitive.hex.sum.leastcost.contrib.to =  aggregate(source_patch_centroid_info$sensitive.leastcost.contrib.to[source_patch_centroid_info$in.landscape == T], 
+  # #                                           by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){
   # #                                             sqrt(sum(x))}) %>% 
   # #   rename(grid_id = Group.1, hex.eca = x)
-  # # sensitive.hex.sum.scaled.leastcost.contrib.to =  aggregate(bl.patch.hexid.centroids$sensitive.leastcost.contrib.to[bl.patch.hexid.centroids$in.landscape == T], 
-  # #                                                  by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){
+  # # sensitive.hex.sum.scaled.leastcost.contrib.to =  aggregate(source_patch_centroid_info$sensitive.leastcost.contrib.to[source_patch_centroid_info$in.landscape == T], 
+  # #                                                  by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){
   # #                                                    sqrt(sum(x))}) %>% 
   # #   rename(grid_id = Group.1, hex.eca = x)
-  # # sensitive.hex.sum.euclid.contrib.to =  aggregate(bl.patch.hexid.centroids$sensitive.euclid.contrib.to[bl.patch.hexid.centroids$in.landscape == T], 
-  # #                                        by = list(bl.patch.hexid.centroids$grid_id[bl.patch.hexid.centroids$in.landscape == T]), function(x){
+  # # sensitive.hex.sum.euclid.contrib.to =  aggregate(source_patch_centroid_info$sensitive.euclid.contrib.to[source_patch_centroid_info$in.landscape == T], 
+  # #                                        by = list(source_patch_centroid_info$grid_id[source_patch_centroid_info$in.landscape == T]), function(x){
   # #                                          sqrt(sum(x))}) %>% 
   # #   rename(grid_id = Group.1, hex.eca = x)
   # 
@@ -179,7 +210,7 @@ for (this.year in years.considered) {
   save(ts.hexgrid,
        landscape.metrics, 
        # sensitive.landscape.leastcost.ECA, sensitive.landscape.scaled.leastcost.ECA, sensitive.landscape.euclid.ECA,
-       bl.patch.hexid.centroids,
+       source_patch_centroid_info,
        file = 
          paste0(func.conect.path, 
                 "\\analysis outputs\\All treescapes\\", this.year, "\\r_funcconnect_EffectiveAreas_ECAobs_.RData")
