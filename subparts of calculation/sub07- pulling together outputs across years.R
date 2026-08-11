2b98e5d45f5fbab6647725720af5cdece03b9d55
EwanMcHenry <ewan.mchenry@outlook.com>
2026-08-11 16:41:34 +0100

    rename and repurpose of script

---
+++ subparts of calculation/sub07- pulling together outputs across years.R
@@ -0,0 +1,54 @@
+##------ Wed Jul 20 12:32:04 2022 ------##
+# Ewan McHenry
+
+multi_year_landscape_summary <- list()
+multi_year_ts.hexgrid <- list()
+multi_year_candidate_pairs <- list()
+multi_year_source_patch_centroid_info <- list()
+
+
+
+
+for (y in seq_along(years.considered)) {
+  this.year <- years.considered[y]
+  
+  load(paste0(func.conect.path, 
+              "\\analysis outputs\\", this.tss[this.ts.num], "\\", this.year, 
+              "\\lcmres", constants$lcm.res, 
+              "\\06_r_funcconnect_EffectiveAreas_ECAobs_.RData"))
+  
+  multi_year_landscape_summary[[y]] <- 
+    landscape_stats[[this.tss[this.ts.num]]][[this.year]]$landscape.summary
+  
+  multi_year_ts.hexgrid[[y]] <- landscape_stats[[this.tss[this.ts.num]]][[this.year]]$ts.hexgrid
+  multi_year_source_patch_centroid_info[[y]] <- landscape_stats[[this.tss[this.ts.num]]][[this.year]]$source_patch_centroid_info
+  multi_year_candidate_pairs[[y]] <- landscape_stats[[this.tss[this.ts.num]]][[this.year]]$candidate_pairs
+  model_params <- landscape_stats[[this.tss[this.ts.num]]][[this.year]]$model_params
+}
+
+multi_year_landscape_summary <- do.call(rbind, multi_year_landscape_summary)
+
+multi_year_results <- list(
+  landscape_summary = multi_year_landscape_summary,
+  ts.hexgrid = multi_year_ts.hexgrid,
+  candidate_pairs = multi_year_candidate_pairs,
+  source_patch_centroid_info = multi_year_source_patch_centroid_info,
+  model_params = model_params
+)
+
+save(
+  multi_year_results,
+  file = paste0(
+    func.conect.path,
+    "\\analysis outputs\\",
+    this.tss[this.ts.num],
+    "\\07_multi_year_landscape_output_lcmres",
+    constants$lcm.res,
+    "_years_",
+    paste(years.considered, collapse = "-"),
+    ".RData"
+  )
+)
+
+
+print(paste(this.tss[this.ts.num], "Pulling across years (script07) done"))
